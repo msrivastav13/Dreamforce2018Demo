884d275816664d07d462a80eaf8a4cd2128c0b26
Mohith Shrivastava <mohith@codescience.com>
2018-10-12 00:16:48 -0400

    update generate package xml shell script

--- scripts/generatePkgXML.sh
+++ scripts/generatePkgXML.sh
@@ -2,14 +2,22 @@
 
 if [ $# -lt 1 ]
 then
-    echo Usage: generatePkgXML.sh orgalias packageName
+    echo Usage: generatepkgXML.sh orgalias packageName
     exit
 fi
 
 ## Retrieve the PackageXML from Unmanaged Container
 
-sfdx force:mdapi:retrieve -s -r ./mdapipkg -u $1 -p "$2"
-unzip -o -q ./mdapipkg/unpackaged.zip -d ./mdapipkg
-cp -a ./mdapipkg/package.xml ./manifest/
+sfdx force:mdapi:retrieve -s -r ./mdapipkg -u $1 -p "$2" # Retrieve Metadata API Source from Package Name
+
+unzip -o -qq ./mdapipkg/unpackaged.zip -d ./mdapipkg # Unzip the file
+
+rm -rf ./manifest/ # If manifest directory exists delete it
+
+mkdir ./manifest/ # Create a New Manifest Directory
+
+cp -a ./mdapipkg/package.xml ./manifest/ # Copy package.XML to manifest directory
+
+rm -rf ./mdapipkg # Delete the mdapipkg source
 
 
